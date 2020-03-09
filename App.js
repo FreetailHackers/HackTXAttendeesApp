c1e87ab5af3655c149c686c63e144850f3bb6155
Ryan Chhong <ryanchhong@gmail.com>
2020-03-09 01:36:25 -0500

    Added all tabs

--- App.js
+++ App.js
@@ -21,14 +21,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './views/HomeScreen'
 import AnnouncementsScreen from './views/AnnouncementsScreen';
 import MyProfileScreen from './views/MyProfileScreen';
+import CalendarScreen from './views/CalendarScreen';
+import MapScreen from './views/MapScreen';
 
 const Tab = createBottomTabNavigator();
 
 function MyStack() {
   return (
     <Tab.Navigator>
+      <Tab.Screen name="Map" component={CalendarScreen} />
       <Tab.Screen name="Announcements" component={AnnouncementsScreen} />
       <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Calendar" component={CalendarScreen} />
       <Tab.Screen name="My Profile" component={MyProfileScreen} />
     </Tab.Navigator>
   );
---
+++ views/CalendarScreen.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import { StatusBar, SafeAreaView, Text } from 'react-native'
+
+export default class CalendarScreen extends Component {
+    // Actual login stuff
+    // Login sends request to verify in normal manner (check user exists + bcrypt)
+    // If valid login, create randon ass key
+    render() {
+        return (
+            <>
+                <StatusBar barStyle='dark-content' />
+                <SafeAreaView style={{flex : 1, backgroundColor: "#00FF00"}}>
+                    <Text>my profile</Text>
+                </SafeAreaView>
+            </>
+        );
+    }
+}
