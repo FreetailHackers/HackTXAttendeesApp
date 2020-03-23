7827b2aa4fd5f0e5febff68c675857b6b4a139ec
Ryan Chhong <ryanchhong@gmail.com>
2020-03-23 14:01:45 -0500

    barebones login stuff working

---
+++ server/application/models.py
@@ -0,0 +1,12 @@
+from . import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    _id = db.Column(db.String(36), primary_key=True)
+    email = db.Column(db.String(64), index=False, unique=True, nullable=False)
+    password = db.Column(db.String(250), index=False, unique=False, nullable=False)
+    auth_token = db.Column(db.String(64), index=False, unique=True, nullable=False)
+    pushed = db.Column(db.DateTime, index=False, unique=False, nullable=False)
+    
+    def __repr__(self):
+        return '<User id={} email={} password={} auth_token={} pushed={}>'.format(_id, email, password, auth_token, pushed)
--- views/MyProfileScreen.js
+++ views/MyProfileScreen.js
@@ -1,16 +1,55 @@
 import React, {Component} from 'react'
-import { StatusBar, SafeAreaView, Text } from 'react-native'
+import { StatusBar, SafeAreaView, Text, TextInput, Button} from 'react-native'
 
 export default class MyProfileScreen extends Component {
     // Actual login stuff
     // Login sends request to verify in normal manner (check user exists + bcrypt)
     // If valid login, create randon ass key
+    constructor(props) {
+        super(props);
+        this.state = {
+            email : "",
+            password : ""
+        };
+    }
+
+    handleInput(field, text) {
+        this.setState({
+            [field] : text, 
+        });
+    }
+
+    handleSubmit() {
+        const payload = {email : this.state.email, password: this.state.password};
+        this.getToken(payload)
+
+    }
+
+    async getToken(payload) {
+        // NOTE: Replace with Local IP of device if using physical
+        await fetch("http://10.0.2.2:5000/login", {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body : JSON.stringify(payload)
+
+        }).then((data) => data.json())
+        .then((data) => {
+            console.log(data);
+        })
+    }
+
     render() {
         return (
             <>
                 <StatusBar barStyle='dark-content' />
                 <SafeAreaView style={{flex : 1, backgroundColor: "#00FF00"}}>
-                    <Text>my profile</Text>
+                    <Text style={{textAlign: "center"}}>Login</Text>
+                    <TextInput style={{borderWidth: 2}} onChangeText={this.handleInput.bind(this, 'email')} value={this.state.email} />
+                    <TextInput style={{borderWidth: 2}} onChangeText={this.handleInput.bind(this, 'password')} value={this.state.password} />
+                    <Button title="Submit" onPress={this.handleSubmit.bind(this)} />
                 </SafeAreaView>
             </>
         );
